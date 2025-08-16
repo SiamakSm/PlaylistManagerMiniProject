21f3234110c188a736eff935e372f11c861755cb
SiamakSm <siamak036@gmail.com>
2025-08-16 18:04:15 +0200

    Audio Playlist Manager Mini Project - First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const fileInput = document.getElementById("fileInput");
+const pickBtn = document.getElementById("pickBtn");
+const dropZone = document.getElementById("dropZone");
+const playlist = document.getElementById("playlist")
+const player = document.getElementById("player")
+const errorBox = document.getElementById("errorBox");
